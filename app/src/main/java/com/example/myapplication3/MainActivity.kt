71ebed6b3773bb2b5a9ac19d50d454527416719e
chltmdgh522 <chltmdgh517@naver.com>
2025-11-12 15:55:53 +0900

    색상 적용이 안될떄

--- app/src/main/java/com/example/myapplication3/MainActivity.kt
+++ app/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -8,11 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
-import com.example.myapplication3.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+//    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
